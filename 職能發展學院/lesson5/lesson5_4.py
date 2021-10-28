307a3fb18c4b94d7f590c37ec7601beeb377d2b9
roberthsu2003 <roberthsu2003@gmail.com>
2021-10-28 11:10:50 +0800

    modify

--- 職能發展學院/lesson5/lesson5_4.py
+++ 職能發展學院/lesson5/lesson5_4.py
@@ -9,9 +9,9 @@ class Window(tk.Tk):
         self.option_add('*font',('verdana',20))
         self.geometry("500x200")
         fm = tk.Frame(self,bg="#aaaaaa")
-        tk.Button(fm, text="LEFT",padx=10,pady=5).pack(expand=tk.YES,fill=tk.BOTH)
-        tk.Button(fm, text="This is the Center Button",padx=10,pady=5).pack(expand=tk.YES,fill=tk.BOTH)
-        tk.Button(fm, text="RIGHT",padx=10,pady=5).pack(expand=tk.YES,fill=tk.BOTH)
+        tk.Button(fm, text="LEFT",padx=10,pady=5).pack(side=tk.LEFT,expand=tk.YES,fill=tk.BOTH)
+        tk.Button(fm, text="This is the Center Button",padx=10,pady=5).pack(side=tk.LEFT,expand=tk.YES,fill=tk.BOTH)
+        tk.Button(fm, text="RIGHT",padx=10,pady=5).pack(side=tk.LEFT,expand=tk.YES,fill=tk.BOTH)
         fm.pack(expand=tk.YES,fill=tk.BOTH)
 
 if __name__ == "__main__":
