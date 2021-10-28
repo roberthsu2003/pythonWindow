c103d0dac68996a842feaa83cd873670e1130b33
roberthsu2003 <roberthsu2003@gmail.com>
2021-10-28 14:52:45 +0800

    modify

--- 職能發展學院/lesson5/lesson5_5.py
+++ 職能發展學院/lesson5/lesson5_5.py
@@ -6,13 +6,13 @@ class Window(tk.Tk):
         super().__init__()
         self.title("上課作業")
         self.option_add("*font",("verdana", 20))
-        self.geometry("300x200")
+        #self.geometry("300x200")
 
         tk.Label(self,text="三個功能按鈕").pack(pady=20)
         topFrame = tk.Frame(self,bg="#aaaaaa")
-        tk.Button(topFrame,text="LEFT").pack(side=tk.LEFT,expand=tk.YES)
-        tk.Button(topFrame,text="CENTER").pack(side=tk.LEFT,expand=tk.YES)
-        tk.Button(topFrame,text="RIGHT").pack(side=tk.LEFT,expand=tk.YES)
+        tk.Button(topFrame,text="台灣總人口數",padx=5,pady=5).pack(side=tk.LEFT,expand=tk.YES,padx=(20,10),pady=(10,20))
+        tk.Button(topFrame,text="台灣總土地",padx=5,pady=5).pack(side=tk.LEFT,expand=tk.YES,padx=(10,10),pady=(10,20))
+        tk.Button(topFrame,text="台灣總區鄉鎮數",padx=5,pady=5).pack(side=tk.LEFT,expand=tk.YES,padx=(10,20),pady=(10,20))
         topFrame.pack(expand=tk.YES,fill=tk.BOTH)
 
 if __name__ == "__main__":
