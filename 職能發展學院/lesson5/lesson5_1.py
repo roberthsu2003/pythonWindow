4de7b37345a9c4de9a5bfed100e61c0bd2ae758a
roberthsu2003 <roberthsu2003@gmail.com>
2021-10-28 10:11:52 +0800

    modify

--- 職能發展學院/lesson5/lesson5_1.py
+++ 職能發展學院/lesson5/lesson5_1.py
@@ -1,1 +1,16 @@
-print("Hello! Python")
+#pack() layout
+import tkinter as tk
+
+
+class Window(tk.Tk):
+    def __init__(self,title):
+        super().__init__()
+        self.title(title)
+
+        tk.Button(self, text="LEFT",padx=10,pady=5).pack(side=tk.LEFT)
+        tk.Button(self, text="CENTER",padx=10,pady=5).pack(side=tk.LEFT)
+        tk.Button(self, text="RIGHT",padx=10,pady=5).pack(side=tk.LEFT)
+
+if __name__ == "__main__":
+    window = Window("layout1")
+    window.mainloop()
