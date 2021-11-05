31919dc355faa9225766e04edfdee67aef941aa2
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-05 09:15:26 +0800

    modify

--- 職能發展學院/lesson5/dialog/__init__.py
+++ 職能發展學院/lesson5/dialog/__init__.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 
 class Dialog(tk.Toplevel):
-    def __init__(self,root,title):
+    def __init__(self,root,title,total):
         super().__init__(root)
         self.transient(root)
-        self.geometry('600x300')
-        tk.Label(self,text=title).pack()
+        #self.geometry('200x200')
+        tk.Label(self,text=title).pack(pady=20,padx=50)
+        print(total)
+        tk.Label(self,text=f'總人口數:{total}').pack(pady=20,padx=50)
