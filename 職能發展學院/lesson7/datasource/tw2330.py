f8af6dea12b5427a6c5daf5996a490eb941677c8
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-05 13:40:44 +0800

    modify

--- 職能發展學院/lesson7/datasource/tw2330.py
+++ 職能發展學院/lesson7/datasource/tw2330.py
@@ -1,2 +1,10 @@
+import requests
+url = 'https://goodinfo.tw/StockInfo/StockBzPerformance.asp?STOCK_ID=2330'
+
 def get_2330():
-    return "台積電"
+    response = requests.get(url)
+    if response.status_code == 200:
+        response.encoding = "utf-8"
+        print(response.text)
+    else:
+        print("下載失敗")
--- 職能發展學院/lesson7/main.py
+++ 職能發展學院/lesson7/main.py
@@ -2,4 +2,4 @@ from datasource import get_2330
 
 if __name__ == "__main__":
     print("主執行檔")
-    print(get_2330())
+    get_2330()
