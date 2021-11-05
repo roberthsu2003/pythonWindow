5e40d5595411081559ebe6b2405c524964632d65
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-05 14:24:42 +0800

    modify

--- 職能發展學院/lesson7/datasource/tw2330.py
+++ 職能發展學院/lesson7/datasource/tw2330.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 url = 'https://goodinfo.tw/StockInfo/StockBzPerformance.asp?STOCK_ID=2330'
 
 headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
@@ -7,6 +8,9 @@ def get_2330():
     response = requests.get(url,headers=headers)
     if response.status_code == requests.codes.ok:
         response.encoding = "utf-8"
-        print(response.text)
+        bs = BeautifulSoup(response.text)
+        dataList = bs.find('div',attrs={'id':'txtFinDetailData'}).find_all('tr',attrs={'align':'center'})
+        for trTag in dataList:
+            print(trTag.prettify())
     else:
         print("下載失敗")
