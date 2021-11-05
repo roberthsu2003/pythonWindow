e9f7c982ba5e086509471308408a27b89b20039d
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-05 14:01:29 +0800

    modify

--- 職能發展學院/lesson7/datasource/tw2330.py
+++ 職能發展學院/lesson7/datasource/tw2330.py
@@ -1,9 +1,11 @@
 import requests
 url = 'https://goodinfo.tw/StockInfo/StockBzPerformance.asp?STOCK_ID=2330'
 
+headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
+
 def get_2330():
-    response = requests.get(url)
-    if response.status_code == 200:
+    response = requests.get(url,headers=headers)
+    if response.status_code == requests.codes.ok:
         response.encoding = "utf-8"
         print(response.text)
     else:
