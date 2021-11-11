fefeba2225eba01cd2e83146b026edf8cd476075
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-11 11:13:03 +0800

    modify

--- 職能發展學院/lesson7/messageWindow/display.py
+++ 職能發展學院/lesson7/messageWindow/display.py
@@ -3,6 +3,5 @@ import tkinter as tk
 class Display(tk.Toplevel):
     def __init__(self, main,stockName,dataList):
         super().__init__(main)
-        print(stockName)
-        for item in dataList:
-            print(item)
+        tk.Label(self,text=f'{stockName}歷年經營績效查詢').pack(padx=10,pady=10)
+
