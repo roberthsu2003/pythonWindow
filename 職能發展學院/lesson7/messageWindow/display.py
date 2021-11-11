db39f76fcdd320553cb7e553b36cfb8a818a49d2
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-11 11:49:27 +0800

    modify

--- 職能發展學院/lesson7/messageWindow/display.py
+++ 職能發展學院/lesson7/messageWindow/display.py
@@ -3,6 +3,8 @@ import tkinter as tk
 class Display(tk.Toplevel):
     def __init__(self, main,stockName,dataList):
         super().__init__(main)
+        self.yearsText = [secondList[0] for secondList in dataList]
+        print(self.yearsText)
         tk.Label(self,text=f'{stockName}歷年經營績效查詢',font=('arial',20)).pack(padx=10,pady=10)
         #建立buttonsFrame
         buttonsFrame = tk.Frame(self,relief=tk.GROOVE,borderwidth=1,width=300,height=80)
