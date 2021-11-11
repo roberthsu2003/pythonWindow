3933cecc2634264bddca45cff06d30187daeb6cf
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-11 11:25:31 +0800

    modify

--- 職能發展學院/lesson7/messageWindow/display.py
+++ 職能發展學院/lesson7/messageWindow/display.py
@@ -3,5 +3,10 @@ import tkinter as tk
 class Display(tk.Toplevel):
     def __init__(self, main,stockName,dataList):
         super().__init__(main)
-        tk.Label(self,text=f'{stockName}歷年經營績效查詢').pack(padx=10,pady=10)
+        tk.Label(self,text=f'{stockName}歷年經營績效查詢',font=('arial',20)).pack(padx=10,pady=10)
+        #建立buttonsFrame
+        buttonsFrame = tk.Frame(self,relief=tk.GROOVE,borderwidth=1,width=300,height=80)
+        tk.Button(buttonsFrame,text="2021").pack()
+        buttonsFrame.pack_propagate(0)
+        buttonsFrame.pack(padx=20,pady=20)
 
