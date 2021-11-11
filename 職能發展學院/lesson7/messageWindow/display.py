2bab947e877d0ad0afc62beb7a12a10821fefd78
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-11 12:01:12 +0800

    modify

--- 職能發展學院/lesson7/messageWindow/display.py
+++ 職能發展學院/lesson7/messageWindow/display.py
@@ -4,13 +4,11 @@ class Display(tk.Toplevel):
     def __init__(self, main,stockName,dataList):
         super().__init__(main)
         self.yearsText = [secondList[0] for secondList in dataList]
-        print(self.yearsText)
         tk.Label(self,text=f'{stockName}歷年經營績效查詢',font=('arial',20)).pack(padx=10,pady=10)
         #建立buttonsFrame
         buttonsFrame = tk.Frame(self,relief=tk.GROOVE,borderwidth=1,width=300,height=80)
-        for rowIndex in range(4):
-            for columnIndex in range(6):
-                tk.Button(buttonsFrame,text=f"row={rowIndex},column={columnIndex}").grid(row=rowIndex,column=columnIndex)
+        for yearIndex,year in enumerate(self.yearsText):
+            print(f'{yearIndex},{year}')
         buttonsFrame.pack_propagate(0)
         buttonsFrame.pack(padx=20,pady=20)
 
