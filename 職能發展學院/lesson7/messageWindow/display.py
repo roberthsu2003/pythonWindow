c8eb66f4d237d7ba2f234c14703f651e28e9a495
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-11 11:37:04 +0800

    modify

--- 職能發展學院/lesson7/messageWindow/display.py
+++ 職能發展學院/lesson7/messageWindow/display.py
@@ -6,7 +6,9 @@ class Display(tk.Toplevel):
         tk.Label(self,text=f'{stockName}歷年經營績效查詢',font=('arial',20)).pack(padx=10,pady=10)
         #建立buttonsFrame
         buttonsFrame = tk.Frame(self,relief=tk.GROOVE,borderwidth=1,width=300,height=80)
-        tk.Button(buttonsFrame,text="2021").pack()
+        for rowIndex in range(4):
+            for columnIndex in range(6):
+                tk.Button(buttonsFrame,text=f"row={rowIndex},row={columnIndex}").grid(row=rowIndex,column=columnIndex)
         buttonsFrame.pack_propagate(0)
         buttonsFrame.pack(padx=20,pady=20)
 
