d91263eb437a03d38f6afe7ede0cf614d2956aeb
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-11 09:10:36 +0800

    modify

--- 職能發展學院/lesson7/main.py
+++ 職能發展學院/lesson7/main.py
@@ -4,10 +4,14 @@ import tkinter as tk
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        name, allDataList = getStackData(2303)
-        print(name)
-        for item in allDataList:
-            print(item)
+        #name, allDataList = getStackData(2303)
+
+        #建立mainFrame
+        mainFrame = tk.Frame(self,relief=tk.GROOVE,borderwidth=1,width=300)
+        tk.Label(mainFrame,text="台股歷年經營績效查詢",font=("arial",20)).pack()
+
+        mainFrame.pack(padx=20,pady=20)
+
 
 
 if __name__ == "__main__":
