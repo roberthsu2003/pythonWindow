3bd808deb223a4c9f21e41c236d92f9936cccaab
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-11 15:09:06 +0800

    modify

--- 職能發展學院/lesson7/messageWindow/display.py
+++ 職能發展學院/lesson7/messageWindow/display.py
@@ -15,8 +15,26 @@ class Display(tk.Toplevel):
             btn = tk.Button(buttonsFrame,text=year)
             btn.grid(row=rIndex,column=cIndex)
             btn.bind("<Button-1>",self.buttonClick)
-        buttonsFrame.pack_propagate(0)
+        #buttonsFrame.pack_propagate(0)
         buttonsFrame.pack(padx=20,pady=20)
+
+        self.infoContainer = tk.Frame(self,relief=tk.GROOVE,borderwidth=1,width=1300,height=80)
+        #-----顯示value的內容
+        self.displayInfoContent(self.infoContainer,'2011')
+        #-----顯示value內容
+        self.infoContainer.pack_propagate(0)
+        self.infoContainer.pack()
+
+    def displayInfoContent(self,parent,year):
+        self.yearLabel = tk.Label(parent, text=year)
+        self.yearLabel.pack()
+        self.subFrame=tk.Frame(parent)
+        titleLabelList = ['年度', '股本(億)', '財報評分', '收盤', '平均', '漲跌', '漲跌(%)', '營業收入',
+         '營業毛利', '營業利益', '業外損益', '稅後淨利', '營業毛利', '營業利益', '業外損益',
+         '稅後淨利', 'ROE(%)', 'ROA(%)', '稅後EPS', 'EPS年增(元)', 'BPS(元)']
+        for labelIndex,labelText in enumerate(titleLabelList):
+            tk.Label(self.subFrame,text=labelText).grid(row=0,column=labelIndex)
+        self.subFrame.pack()
 
     def buttonClick(self,event):
         pressedBtn = event.widget #取得被按按鈕的參考
