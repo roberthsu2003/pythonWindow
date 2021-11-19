47a52dfb191d757b8f6ca1d7816951fe258d33a6
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-19 15:04:51 +0800

    modify

--- 職能發展學院/lesson11/dataSource.py
+++ 職能發展學院/lesson11/dataSource.py
@@ -1,32 +1,33 @@
 __all__ = ["getAirData"]
 
-url = "https://data.epa.gov.tw/api/v1/aqx_p_432?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&format=json"
+url = "https://data.epa.gov.tw/api/v/aqx_p_432?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&format=json"
 
 def getAirData():
     '''
-    連線空氣品質指標
+    連線行政院環保署空氣品質指標
+    有自已的apiKey
     :return: None
     '''
     import requests
-    from  requests.exceptions import HTTPError
+    from requests.exceptions import HTTPError
     try:
         response = requests.get(url)
         response.raise_for_status()
     except ConnectionError as e:
         print("連線錯誤",e)
-        return
+        return None
     except HTTPError as e:
         print("HTTP錯誤", e)
-        return
+        return None
     except requests.Timeout:
         print("TimeOut")
-        return
+        return None
     except:
         print("其它錯誤")
-        return
+        return None
     else:
-        print("下載成功")
         print(response.text)
+        return "資料"
 
 
 
--- 職能發展學院/lesson11/main.py
+++ 職能發展學院/lesson11/main.py
@@ -1,12 +1,16 @@
 import tkinter as tk
 import dataSource
+from tkinter import messagebox
 
 class Window(tk.Tk):
 
     def __init__(self):
         super().__init__()
         self.title = "全省空氣品質指標"
-        dataSource.getAirData()
+        if dataSource.getAirData() is not None:
+            print("下載成功")
+        else:
+            messagebox.showerror("連線錯誤","請稍後再試")
 
 if __name__ == "__main__":
     window = Window()
