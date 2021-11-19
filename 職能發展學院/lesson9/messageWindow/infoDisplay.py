f4fd1522ec0f590bfaae832b30b32797bcd9b954
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-19 10:35:13 +0800

    modify

--- 職能發展學院/lesson9/messageWindow/infoDisplay.py
+++ 職能發展學院/lesson9/messageWindow/infoDisplay.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 from tkinter.simpledialog import Dialog
 
 class InfoDisplay(Dialog):
@@ -9,6 +10,19 @@ class InfoDisplay(Dialog):
         super().__init__(parent,title)
 
     def body(self,master):
+        topFrame=tk.Frame(master,bg='#999999')
+        leftFrame = tk.Frame(topFrame,width=100,height=200,bg='#aaaaaa')
+        listbox = tk.Listbox(leftFrame)
+        for item in self.info:
+            listbox.insert(tk.END, item[0])
+
+
+        listbox.pack(side=tk.LEFT)
+        leftFrame.pack_propagate(0)
+        leftFrame.pack(side=tk.LEFT)
+        #topFrame.pack_propagate(0)
+        topFrame.pack()
+
         print("master:",master)
         print("傳過來的資料:",self.info)
 
