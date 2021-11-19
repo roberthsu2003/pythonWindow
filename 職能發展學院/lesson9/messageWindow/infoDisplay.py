0c1eb66cff4c58fd9511c5777d4ed516236394e0
roberthsu2003 <roberthsu2003@gmail.com>
2021-11-19 11:49:49 +0800

    modify

--- 職能發展學院/lesson9/messageWindow/infoDisplay.py
+++ 職能發展學院/lesson9/messageWindow/infoDisplay.py
@@ -4,9 +4,10 @@ from tkinter.simpledialog import Dialog
 class InfoDisplay(Dialog):
 
     def __init__(self,parent,title=None,info=None): #自訂的初始化
-        print(title)
+        self.subFrame = None  # subFrame一開始設None,建立一個Display的屬性subFrame
         if info is not None:
             self.info = info
+            self.yearsText = [secondList[0] for secondList in self.info]
         super().__init__(parent,title)
 
     def body(self,master):
@@ -25,9 +26,16 @@ class InfoDisplay(Dialog):
         leftFrame.pack(side=tk.LEFT,fill=tk.Y)
         #topFrame.pack_propagate(0)
 
-        rightFrame = tk.Frame(topFrame, bg='#555555',width=700,height=200)
-        rightFrame.pack(side=tk.RIGHT)
-        rightFrame.pack_propagate(0)
+
+        #右邊資料顯示
+        self.infoContainer = tk.Frame(topFrame, width=700, height=200)
+        # -----顯示value的內容
+        self.displayInfoContent(self.infoContainer, self.yearsText[0])
+        # -----顯示value內容
+        self.infoContainer.pack_propagate(0)
+        self.infoContainer.pack(side=tk.RIGHT)
+
+
         topFrame.pack()
 
         print("master:",master)
@@ -38,6 +46,24 @@ class InfoDisplay(Dialog):
         index = widget.curselection()[0]
         year = widget.get(index)
         print(year)
+
+    def displayInfoContent(self,parent,year):
+        if self.subFrame:
+            self.subFrame.destroy() #如果已經有subFrame,則將subFrame消滅
+
+        self.subFrame=tk.Frame(parent)
+        yearindex = self.yearsText.index(year)
+        valueList = self.info[yearindex] #儲存value內容
+        titleLabelList = ['年度', '股本(億)', '財報評分', '收盤', '平均', '漲跌', '漲跌(%)', '營業收入',
+         '營業毛利', '營業利益', '業外損益', '稅後淨利', '營業毛利', '營業利益', '業外損益',
+         '稅後淨利', 'ROE(%)', 'ROA(%)', '稅後EPS', 'EPS年增(元)', 'BPS(元)']
+        for labelIndex,labelText in enumerate(titleLabelList):
+            columnCount = 11 #顯示欄位數量
+            rowIndex = labelIndex // columnCount
+            columnIndex = labelIndex % columnCount
+            tk.Label(self.subFrame,text=labelText,bg='#999999').grid(row=rowIndex*2 ,column=columnIndex,sticky='ew') #title的文字，分2列,0和1
+            tk.Label(self.subFrame, text=valueList[labelIndex]).grid(row=(rowIndex * 2)+1, column=columnIndex,sticky='ew')  # value的文字，分2列,1和3
+        self.subFrame.pack()
 
 
 
