3a4d1a6fa5b322ebcf59561298e0e7b7fa6a7f84
徐國堂 <roberthsu2003@gmail.com>
2021-12-15 13:34:12 +0800

    modify

--- 實際案例/7-2顯示政府開放平台空氣品質指標正式版/dataSource.py
+++ 實際案例/7-2顯示政府開放平台空氣品質指標正式版/dataSource.py
@@ -1,59 +1,98 @@
-import requests
-from requests import ConnectionError,HTTPError,Timeout
+__all__ = ["getAirData"]
 
-url = "https://data.epa.gov.tw/api/v1/aqx_p_432?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&format=json"
-newRecords = [] # 建立要傳出的list
+
+class CityWeather():
+    def __init__(self,siteName=None,county=None,aqi=None,pm25=None,status=None,time=None):
+        self.siteName = siteName
+        self.county = county
+        self.aqi = aqi
+        self.pm25 = pm25
+        self.status = status
+        self.time = time
+
+
 def getAirData():
-    global newRecords
-    newRecords = []  #由於有更新的動作，所以要全部清空，重新再抓一次
+    '''
+    連線行政院環保署空氣品質指標
+    有自已的apiKey
+    :return: 傳出citys,list內有自訂實體CityWeather
+    '''
+    import requests
+    from requests.exceptions import HTTPError
+    import json
+
+    url = "https://data.epa.gov.tw/api/v1/aqx_p_432?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&format=json"
+
     try:
         response = requests.get(url)
         response.raise_for_status()
-    except ConnectionError:
-        print('找不到伺服器')
-        return None
-    except HTTPError:
-        print('網頁找不到')
-        return None
-    except Timeout:
-        print('超過時間沒有回應')
-        return None
-    else:
-        if response.status_code == 200:
-            print("下載成功")
-        else:
-            print("失敗")
-            return None
+    except ConnectionError as e:
+        raise ValueError("連線錯誤")
+    except HTTPError as e:
+        raise ValueError("HTTP錯誤")
+    except requests.Timeout:
+        raise ValueError("TimeOut")
+    except:
+        raise ValueError("其它錯誤")
+
+    saveFile(response.text) #儲存檔案
+    jsonFilePath = getlastestFilePath() #取得最新儲存檔案的路徑
+
+    #開啟json檔
+    jsonFile = open(jsonFilePath,'r',encoding="utf-8")
+    originJson = json.load(jsonFile) #解析json的file物件，成為python的基本資料結構
+    jsonFile.close()
+    #originJson = response.json() #取得原始資料
+    records = originJson["records"]
+
+    #解析資料
+
+    cities = [CityWeather(siteName=dict["SiteName"],county=dict["County"],aqi=dict["AQI"],pm25=dict["PM2.5"],status=dict["Status"],time=dict["ImportDate"]) for dict in records]
+
+    return cities
+
+def saveFile(fileContent):
+    '''
+    :param fileContent: 要儲存的文字內容
+    :return: None
+    '''
+    import os
+    from datetime import datetime
+    folderName = "data" #資料夾名稱
+    now = datetime.now()
+    fileName = f"{now.year}-{now.month}-{now.day}-{now.hour}-{now.minute}-{now.second}.json" #存檔的檔案名稱
+    savePath = f"./{folderName}/{fileName}"
+    absPath = os.path.abspath(savePath) #存檔的絕對路徑
+
+    #檢查有沒有資料夾, 沒有就建立資料夾
+    if not os.path.exists(folderName):
+        os.mkdir(folderName)
+
+    #儲存檔案
+    file = open(absPath,"wt",encoding="utf-8")
+    file.write(fileContent)
+    file.close()
+    print("存檔完畢")
+
+def getlastestFilePath():
+    '''
+    取得data資料夾內最新的檔案
+    :return:傳出最新json檔的絕對路徑
+    '''
+    import os
+    from datetime import datetime
+    folderName = "data"
+
+    fileNames = os.listdir(folderName)  #取得data資料夾內檔案名稱的list
+    datetimeList = [datetime.strptime(fileName,"%Y-%m-%d-%H-%M-%S.json") for fileName in fileNames] #建立list,裏面是datatime物件
+    datetimeList.sort(reverse=True) #排序datatimes物件，由大到小
+    lestestDateTime = datetimeList[0] #取出最新的dateTime物件
+    #透過datatime物件建立檔案名稱
+    fileName = f"{lestestDateTime.year}-{lestestDateTime.month}-{lestestDateTime.day}-{lestestDateTime.hour}-{lestestDateTime.minute}-{lestestDateTime.second}.json"
+    absFileName=os.path.abspath(f"{folderName}/{fileName}") #取出最新檔案的絕對路徑
+    return absFileName
 
 
-
-    allData = response.json()  # dictinary
-    records = allData['records']  # list
-    for record in records:  # record是dictionary
-        newItem = {}
-        newItem["監測點"] = record["SiteName"]
-        newItem["城市"] = record['County']
-        newItem["AQI"] = record['AQI']
-        newItem["狀態"] = record['Status']
-        newItem["時間"] = record['ImportDate']
-        newRecords.append(newItem)
-
-    return newRecords
-
-def getPositionList():
-    positionList = []
-    for itemDic in newRecords:
-        positionList.append(itemDic['監測點'])
-    return tuple(positionList)
-
-def getOneSiteData(name):
-    for itemDic in newRecords:
-        if itemDic['監測點'] == name:
-            return itemDic
-
-
-
-__all__ = ["newRecords", "getAirData","getPositionList", "getOneSiteData"]
 
 
 
---
+++ 實際案例/7-2顯示政府開放平台空氣品質指標正式版/main.py
@@ -0,0 +1,153 @@
+import tkinter as tk
+import dataSource
+from tkinter import messagebox
+import tkinter.ttk as ttk
+from datetime import datetime,timedelta
+
+class Window(tk.Tk):
+
+    def getInternetData(self):
+        try:
+            self.downloadTime = datetime.now()
+            citylist = dataSource.getAirData()
+            self.cities = {cityObject.county:cityObject for cityObject in citylist} #包存現在要顯示的資料,轉成dict,key=城市名,value=CityWeather的實體
+        except ValueError as e:
+            messagebox.showwarning("連線錯誤",e)
+            self.destroy()
+
+        currentTimeString = citylist[0].time  # 取得json內顯示的時間字串
+
+        self.currentDateTime = datetime.strptime(currentTimeString,
+                                                 "%Y-%m-%d %H:%M:%S.%f")  # 將字串轉為datetime物件,保存目前顯示資料的datetime物件
+
+
+    def __init__(self):
+        super().__init__()
+        self.title("全省空氣品質指標")
+
+        #--------------取得資料start-------------------#
+
+        self.getInternetData()
+
+        # --------------取得資料end-------------------#
+
+
+
+        # --------------建立視窗start-------------------#
+        mainFrame = tk.Frame(self,width=500,height=600,borderwidth=1,relief=tk.GROOVE,padx=20,pady=20)
+
+        #建立上方的topFrame
+        topFrame = tk.Frame(mainFrame)
+        tk.Label(topFrame,text="台灣各地空氣品質指標",font=("arial",22,"bold"),fg="#555555").pack()
+        self.currentTimeLabel = tk.Label(topFrame,text="觀測時間:xxxxxxxxx",font=("arial",16),fg="#555555")
+        self.currentTimeLabel.pack(pady=(30,10))
+        self.nextTimeLabel = tk.Label(topFrame,text="下次更新時間:xxxxxxxxx",font=("arial",16),fg="#555555")
+        self.nextTimeLabel.pack()
+        self.leftTimeLabel = tk.Label(topFrame, text="20:15", font=("arial", 16), fg="#555555")
+        self.leftTimeLabel.pack()
+        topFrame.pack()
+
+        #建立中間的middleFrame
+        middleFrame = tk.Frame(mainFrame)
+        tk.Label(middleFrame,text="請選擇監測站:",font=("arial",20),fg="#555555").pack(side=tk.LEFT)
+        self.comboboxText = tk.StringVar()
+        self.comboBox = ttk.Combobox(middleFrame,textvariable=self.comboboxText,font=("arial",20))
+        self.comboBox.pack(side=tk.LEFT)
+        self.comboBox.bind('<<ComboboxSelected>>', self.combobox_selected)
+        middleFrame.pack(pady=20)
+
+        #建立下方的bottomFrame
+        bottomFrame = tk.Frame(mainFrame,bg='#666666')
+        bottomFrame.columnconfigure(0,weight=1)
+        bottomFrame.columnconfigure(1, weight=2)
+        tk.Label(bottomFrame,text="監測點:",font=("Arial",16),bg='#666666',fg='#ffffff').grid(row=0,column=0,sticky=tk.E,padx=10,pady=10)
+        self.siteNameLabel = tk.Label(bottomFrame,text="監測點",font=("Arial",16),bg='#666666',fg='#ffffff')
+        self.siteNameLabel.grid(row=0,column=1,sticky=tk.W,padx=10,pady=10)
+
+        tk.Label(bottomFrame, text="城市:", font=("Arial", 16),bg='#666666',fg='#ffffff').grid(row=1, column=0, sticky=tk.E, padx=10, pady=10)
+        self.cityNameLabel = tk.Label(bottomFrame, text="城市", font=("Arial", 16),bg='#666666',fg='#ffffff')
+        self.cityNameLabel.grid(row=1, column=1, sticky=tk.W,padx=10, pady=10)
+
+        tk.Label(bottomFrame, text="AQI:", font=("Arial", 16),bg='#666666',fg='#ffffff').grid(row=2, column=0, sticky=tk.E, padx=10, pady=10)
+        self.aqiLabel = tk.Label(bottomFrame, text="AQI", font=("Arial", 16),bg='#666666',fg='#ffffff')
+        self.aqiLabel.grid(row=2, column=1, sticky=tk.W,padx=10, pady=10)
+
+        tk.Label(bottomFrame, text="PM2.5:", font=("Arial", 16),bg='#666666',fg='#ffffff').grid(row=3, column=0, sticky=tk.E, padx=10, pady=10)
+        self.pm25Label = tk.Label(bottomFrame, text="PM2.5", font=("Arial", 16),bg='#666666',fg='#ffffff')
+        self.pm25Label.grid(row=3, column=1, sticky=tk.W,padx=10, pady=10)
+
+        tk.Label(bottomFrame, text="狀態:", font=("Arial", 16),bg='#666666',fg='#ffffff').grid(row=4, column=0, sticky=tk.E, padx=10, pady=10)
+        self.stateLabel = tk.Label(bottomFrame, text="狀態", font=("Arial", 16),bg='#666666',fg='#ffffff')
+        self.stateLabel.grid(row=4, column=1, sticky=tk.W,padx=10, pady=10)
+
+        bottomFrame.pack(pady=20,padx=70,fill=tk.X,expand=tk.YES,anchor=tk.CENTER)
+
+
+        mainFrame.pack_propagate(0)
+        mainFrame.pack(padx=50,pady=50)
+        # --------------建立視窗end-------------------#
+
+        #-------------更新標題start---------#
+        self.updateTopWindowContent(self.currentDateTime,self.downloadTime,list(self.cities.keys()))
+        # -------------更新標題end---------#
+
+    def updateTopWindowContent(self,displayCurrent,downloadTime,cityNames):
+        '''
+        更新畫面上方的的內容
+        :param displayCurrent: 觀測的時間(datetime物件)
+        :param downloadTime: 現在下載的時間(datetime物件)
+        :param cityNames:目前所有城市名程(list,包含城市名稱字串)
+        :return:
+        '''
+        self.currentTimeLabel.config(text=displayCurrent.strftime("觀測時間:%Y年%m月%d日--%H時%M分%S秒"))
+        self.updateTime = downloadTime + timedelta(minutes=10);
+        self.updatePersecond()
+        self.nextTimeLabel.config(text=self.updateTime.strftime("下次更新時間:%Y年%m月%d日--%H時%M分%S秒"))
+        self.comboBox.config(values=cityNames)
+        self.comboBox.current(0) #預設選擇第一位
+        self.updateBottomWindowContent(cityNames[0])#預設選擇第一個城市
+
+    def updatePersecond(self):
+        '''
+        每隔1秒執行1次,更新self.leftTimeLabel的text
+        時間到時，重新下載,更新畫面，取消self.updateTime
+        :return:
+        '''
+        remainTime = self.updateTime - datetime.now()  # 下載時間 - 現在既時時間 #timeDelta實體
+        if remainTime.seconds <= 0:
+            self.getInternetData() #重新下載資料
+            self.updateTopWindowContent(self.currentDateTime,self.downloadTime,list(self.cities.keys())) #更新上方畫面
+            print('時間到了')
+            return #取消self.updateTime
+
+        remainSeconds = remainTime.seconds #取出秒數
+        remainMinutes = remainSeconds // 60 #轉換為分鐘
+        remainSeconds1 = remainSeconds % 60 #轉換為剩餘秒數
+        self.leftTimeLabel.config(text=f"剩餘:{remainMinutes}分,{remainSeconds1}秒")
+        self.after(1000,self.updatePersecond)
+
+    def updateBottomWindowContent(self,selectedCounty):
+        '''
+        更新視窗下半部內容
+        :param selectedCounty: 使用者選擇的城市string
+        :return:
+        '''
+        cityWeather = self.cities[selectedCounty]
+        self.siteNameLabel.config(text=cityWeather.siteName)
+        self.cityNameLabel.config(text=cityWeather.county)
+        self.aqiLabel.config(text=cityWeather.aqi)
+        self.pm25Label.config(text=cityWeather.pm25)
+        self.stateLabel.config(text=cityWeather.status)
+
+
+    def combobox_selected(self,event):
+        widget = event.widget
+        comboBoxIndex = widget.current()
+        selectedCounty = self.comboboxText.get()
+        self.updateBottomWindowContent(selectedCounty)
+
+
+
+if __name__ == "__main__":
+    window = Window()
+    window.mainloop()
