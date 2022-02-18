f465b50fc4620b7d7d1ebb40003c092a4ab1c28a
徐國堂 <roberthsu2003@gmail.com>
2022-02-18 14:23:32 +0800

    modify

--- 職能發展學院/lesson3_2.py
+++ 職能發展學院/lesson3_2.py
@@ -10,14 +10,18 @@ class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         labelFont = tkFont.Font(family="Arial", size=30)
-        label = ttk.Label(self,text='Hello World',font=labelFont)
-        label.pack(expand=True,fill=tk.X,anchor=tk.CENTER,padx=20)
+        label = ttk.Label(self,text='Hello World',font=labelFont,anchor=tk.CENTER)
+        label.pack(padx=20,pady=10)
+        leftbutton = ttk.Button(self,text="確定")
+        leftbutton.pack(side=tk.LEFT,pady=10,ipady=10,padx=20)
+        rightbutton = ttk.Button(self, text="取消")
+        rightbutton.pack(side=tk.RIGHT, pady=10, ipady=10,padx=20)
 
 
 
 if __name__ == "__main__":
     window = Window()
     window.title('class的寫法')
-    window.geometry('300x300')
+    #window.geometry('300x300')
     window.config(bg='blue')
     window.mainloop()
