82adecfe293efc551f71bc7fecd47bcd3ade8085
徐國堂 <roberthsu2003@gmail.com>
2022-02-18 14:38:20 +0800

    modify

--- 職能發展學院/lesson3_2.py
+++ 職能發展學院/lesson3_2.py
@@ -9,13 +9,25 @@ from tkinter import ttk
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
+        topFrame = ttk.LabelFrame(self,text="上方選項")
         labelFont = tkFont.Font(family="Arial", size=30)
-        label = ttk.Label(self,text='Hello World',font=labelFont,anchor=tk.CENTER)
+        label = ttk.Label(topFrame,text='Hello World',font=labelFont,anchor=tk.CENTER)
         label.pack(padx=20,pady=10)
-        leftbutton = ttk.Button(self,text="確定")
+        leftbutton = ttk.Button(topFrame,text="確定")
         leftbutton.pack(side=tk.LEFT,pady=10,ipady=10,padx=20)
-        rightbutton = ttk.Button(self, text="取消")
+        rightbutton = ttk.Button(topFrame, text="取消")
         rightbutton.pack(side=tk.RIGHT, pady=10, ipady=10,padx=20)
+        topFrame.pack(padx=10,pady=10)
+
+        buttonFrame = ttk.LabelFrame(self, text="下方選項")
+        labelFont = tkFont.Font(family="Arial", size=30)
+        label1 = ttk.Label(buttonFrame, text='Hello World', font=labelFont, anchor=tk.CENTER)
+        label1.pack(padx=20, pady=10)
+        leftbutton1 = ttk.Button(buttonFrame, text="確定")
+        leftbutton1.pack(side=tk.LEFT, pady=10, ipady=10, padx=20)
+        rightbutton1 = ttk.Button(buttonFrame, text="取消")
+        rightbutton1.pack(side=tk.RIGHT, pady=10, ipady=10, padx=20)
+        buttonFrame.pack(padx=10, pady=10)
 
 
 
@@ -23,5 +35,5 @@ if __name__ == "__main__":
     window = Window()
     window.title('class的寫法')
     #window.geometry('300x300')
-    window.config(bg='blue')
+    #window.config(bg='blue')
     window.mainloop()
