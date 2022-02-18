676133abce3ce9dc45ab4e0bd28e945506d89e9b
徐國堂 <roberthsu2003@gmail.com>
2022-02-18 11:05:14 +0800

    modify

--- 職能發展學院/lesson3_1.py
+++ 職能發展學院/lesson3_1.py
@@ -3,12 +3,15 @@
 這是我的python_window範例
 """
 import tkinter as tk
+from tkinter import ttk
 
 def createdWindow():
     window = tk.Tk()
     window.title("我的第一個視窗")
     window.geometry('600x300+200+200')
     window.resizable(False,False)
+    label = ttk.Label(window,text="Hello World")
+    label.pack(fill=tk.BOTH, expand=1, padx=100, pady=100)
     window.mainloop()
 
 if __name__ == "__main__":
