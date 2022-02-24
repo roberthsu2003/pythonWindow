ed411b2c5c44d50531080ec54e2e5c6223146df6
徐國堂 <roberthsu2003@gmail.com>
2022-02-24 10:10:57 +0800

    modify

--- 職能發展學院/lesson4/lesson4_1.py
+++ 職能發展學院/lesson4/lesson4_1.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3.10
+import requests
+
 urlpath = '	https://data.epa.gov.tw/api/v1/aqx_p_02?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&sort=ImportDate%20desc&format=json'
+def downloadData():
+    response = requests.get(urlpath)
+    if response.status_code == 200:
+        print('下載成功')
+        data = response.json()
+        print(data)
+
 def main():
-    pass
+    downloadData()
 
 if __name__ == '__main__':
     main()
