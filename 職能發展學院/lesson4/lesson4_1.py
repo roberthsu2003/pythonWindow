33ad85978b131b41381680f4f27123b47a35bf38
徐國堂 <roberthsu2003@gmail.com>
2022-02-24 09:52:18 +0800

    modify

---
+++ 職能發展學院/lesson4/lesson4_1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.10
+urlpath = '	https://data.epa.gov.tw/api/v1/aqx_p_02?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&sort=ImportDate%20desc&format=json'
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
