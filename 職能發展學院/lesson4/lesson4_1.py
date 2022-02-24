b17ee54bab9d50d98a6223abda6d2b6a68dc2ead
徐國堂 <roberthsu2003@gmail.com>
2022-02-24 11:11:12 +0800

    modify

--- 職能發展學院/lesson4/lesson4_1.py
+++ 職能發展學院/lesson4/lesson4_1.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3.10
 import requests
+import sqlite3
+from sqlite3 import Error
 
 urlpath = '	https://data.epa.gov.tw/api/v1/aqx_p_02?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&sort=ImportDate%20desc&format=json'
+
+def create_connection(db_file):
+    """
+    連線至資料庫
+    :param db_file: 資料庫的檔案名稱
+    :return: Connection物件
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+        return
+
+    return conn
 
 def stringToFloat(s):
     try:
@@ -15,8 +33,8 @@ def saveToDataBase(datas):
     :param datas: list->tuple
     :return:
     '''
-    for item in datas:
-        print(item)
+    conn = create_connection('pm25.db')
+    print("資料庫連線成功")
 
 def downloadData():
     response = requests.get(urlpath)
@@ -27,10 +45,11 @@ def downloadData():
         importData = [
             (item['Site'], item['county'], stringToFloat(item['PM25']), item['DataCreationDate'], item['ItemUnit']) for
             item in datas]
-        saveToDataBase(importData)
+        return importData
 
 def main():
-    downloadData()
+    importData = downloadData()
+    saveToDataBase(importData)
 
 if __name__ == '__main__':
     main()
