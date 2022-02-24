169d9db28e5cc8b064571e0d45f14973dcfbead5
徐國堂 <roberthsu2003@gmail.com>
2022-02-24 16:03:35 +0800

    modify

--- 職能發展學院/lesson4/lesson4.py
+++ 職能發展學院/lesson4/lesson4.py
@@ -1,12 +1,30 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter import font
 import dataSource
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
+        #title_Font = font.nametofont('TkCaptionFont')
+        title_Font = font.Font(family='Helvetica', size=20, weight='bold')
+        titleLabel = ttk.Label(self, text="台灣即時PM2.5",font=title_Font,anchor=tk.CENTER)
+        titleLabel.pack(fill=tk.X, pady=20)
 
-        label = tk.Label(self, text="Hello World!")
-        label.pack(fill=tk.BOTH, expand=1, padx=100, pady=50)
+        cityLabel = ttk.Label(self, text="城市:")
+        cityLabel.pack(side=tk.LEFT,padx=(50,0))
+
+        cityvar = tk.StringVar()
+        city_combobox = ttk.Combobox(self, textvariable=cityvar)
+        city_combobox.pack(side=tk.LEFT)
+
+        siteLabel = ttk.Label(self, text="站點:")
+        siteLabel.pack(side=tk.LEFT, padx=(50, 0))
+
+        site_listbox = tk.Listbox(self, height=10)
+        site_listbox.pack(side=tk.LEFT,padx=(0,50),pady=(0,30))
+
+
 
 
 if __name__ == "__main__":
