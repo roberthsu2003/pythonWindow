04e89cc75beb55394584c44bdadd6a7351e0b2d5
徐國堂 <roberthsu2003@gmail.com>
2022-03-11 14:36:03 +0800

    modify

--- 職能發展學院/youbikeTP/youbike.py
+++ 職能發展學院/youbikeTP/youbike.py
@@ -8,8 +8,19 @@ class Window(tk.Tk):
         #上方的Frame=========start
         topFrame = tk.Frame(self,background='red')
         tk.Label(topFrame,text="台北市youbike即時監測系統",font=("arial",20)).pack()
-        topFrame.grid(column=0,row=0,padx=20,pady=20)
+        topFrame.grid(column=0,row=0,columnspan=3,padx=20,pady=20)
         #上方的Frame=========end
+        leftBottom = tk.LabelFrame(self,background='blue')
+        tk.Label(leftBottom, text="正常租借站點", font=("arial", 20)).pack()
+        leftBottom.grid(column=0,row=1,padx=20,pady=20)
+
+        centerBottom = tk.LabelFrame(self,background='gray')
+        tk.Label(centerBottom, text="將無車可借站點", font=("arial", 20)).pack()
+        centerBottom.grid(column=1,row=1,padx=20,pady=20)
+
+        rightBottom = tk.LabelFrame(self, background='green')
+        tk.Label(rightBottom, text="將無法還車站點", font=("arial", 20)).pack()
+        rightBottom.grid(column=2, row=1, padx=20, pady=20)
 
 if __name__=="__main__":
     dataSource.update_youbike_data()
