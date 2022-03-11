c0da7b8c3d24c53c333ff9fb90d88b64c3aed666
徐國堂 <roberthsu2003@gmail.com>
2022-03-11 14:20:58 +0800

    modify

--- 職能發展學院/youbikeTP/youbike.py
+++ 職能發展學院/youbikeTP/youbike.py
@@ -5,6 +5,11 @@ from tkinter import ttk
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
+        #上方的Frame=========start
+        topFrame = tk.Frame(self,background='red')
+        tk.Label(topFrame,text="台北市youbike即時監測系統",font=("arial",20)).pack()
+        topFrame.grid(column=0,row=0,padx=20,pady=20)
+        #上方的Frame=========end
 
 if __name__=="__main__":
     dataSource.update_youbike_data()
