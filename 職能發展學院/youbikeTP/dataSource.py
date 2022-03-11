4c2d1f4089a78be1222ca787ae6e840fae5bab3b
徐國堂 <roberthsu2003@gmail.com>
2022-03-11 09:37:14 +0800

    modify

--- 職能發展學院/youbikeTP/dataSource.py
+++ 職能發展學院/youbikeTP/dataSource.py
@@ -1,1 +1,26 @@
-youbikeurl = "https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json"
+import requests
+from requests import ConnectionError,HTTPError,Timeout
+
+def download_youbike_data():
+    youbikeurl = "https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json"
+    try:
+        response = requests.get(youbikeurl)
+        response.raise_for_status()
+    except ConnectionError as e:
+        print("網路連線有問題")
+        print(e)
+        return
+    except HTTPError as e:
+        print("statusCode不是200,連線取得資料有問題")
+        print(e)
+        return
+    except Timeout as e:
+        print("伺服器忙線中")
+        print(e)
+        return
+    except:
+        print("不預期的錯誤")
+        return
+
+    allData = response.json()
+    print(allData)
---
+++ 職能發展學院/youbikeTP/youbike.py
@@ -0,0 +1,4 @@
+import dataSource
+
+if __name__=="__main__":
+    dataSource.download_youbike_data()
