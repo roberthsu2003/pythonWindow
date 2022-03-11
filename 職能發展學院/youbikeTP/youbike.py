62e13c0399c4125a769e043aac265e5e7e1b72ea
徐國堂 <roberthsu2003@gmail.com>
2022-03-11 14:00:58 +0800

    modify

--- 職能發展學院/youbikeTP/youbike.py
+++ 職能發展學院/youbikeTP/youbike.py
@@ -1,4 +1,13 @@
 import dataSource
+import tkinter as tk
+from tkinter import ttk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
 if __name__=="__main__":
     dataSource.update_youbike_data()
+    window = Window()
+    window.title("台北市youbike及時監測資料")
+    window.mainloop()
