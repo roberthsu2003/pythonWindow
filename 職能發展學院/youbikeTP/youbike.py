f49069f8ecc0fbcac3121f1f1ca2a84ed3aceb89
徐國堂 <roberthsu2003@gmail.com>
2022-03-11 16:07:51 +0800

    modify

--- 職能發展學院/youbikeTP/youbike.py
+++ 職能發展學院/youbikeTP/youbike.py
@@ -10,7 +10,7 @@ class Window(tk.Tk):
         tk.Label(topFrame,text="台北市youbike即時監測系統",font=("arial",20)).pack()
         topFrame.grid(column=0,row=0,columnspan=3,padx=20,pady=20)
         #上方的Frame=========end
-        LeftLabelFrame(self,background='blue',text="左邊的").grid(column=0,row=1,padx=20,pady=20)
+        LeftLabelFrame(self,text="左邊的").grid(column=0,row=1,padx=20,pady=20)
         CenterLabelFrame(self,background='red',text="中間的").grid(column=1,row=1,padx=20,pady=20)
         RightLabelFrame(self, background='green',text="右邊的").grid(column=2, row=1, padx=20, pady=20)
 
@@ -21,7 +21,18 @@ class LeftLabelFrame(tk.LabelFrame):
         tk.Label(topFrame, text="正常租借站點", font=("arial", 20),background='gray',fg="white").pack(padx=10,pady=10)
         normal_count = dataSource.get_count_of_normal()
         tk.Label(topFrame, text=f"數量:{normal_count}",background='gray',fg='#ffffff', font=("arial",20)).pack(padx=10,pady=10)
-        topFrame.pack()
+        topFrame.pack(pady=20)
+        treeView = ttk.Treeview(self,columns=('sna','tot','sbi','bemp'),show="headings")
+        treeView.heading('sna',text='名稱')
+        treeView.heading('tot', text='總數')
+        treeView.heading('sbi', text='可借')
+        treeView.heading('bemp', text='可還')
+
+        treeView.column('sna',width=300)
+        treeView.column('tot',width=50)
+        treeView.column('sbi',width=50)
+        treeView.column('bemp',width=50)
+        treeView.pack()
 
 class CenterLabelFrame(tk.LabelFrame):
     def __init__(self, *args , **kwargs):
