e70663bf47a7e41514d20a184ae22c7139548822
徐國堂 <roberthsu2003@gmail.com>
2022-05-16 16:18:54 +0800

    modify

--- 職能發展學院/n1_taipei_youbike/main.py
+++ 職能發展學院/n1_taipei_youbike/main.py
@@ -1,1 +1,13 @@
-print(__name__)
+def func1():
+    print("func1")
+
+def func2():
+    print("func2")
+
+def func3():
+    print("func3")
+
+if __name__ == "__main__":
+    func3()
+    func2()
+    func1()
