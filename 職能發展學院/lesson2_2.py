ff9d1d007be4a0c60172070653829460be2e35fb
徐國堂 <roberthsu2003@gmail.com>
2022-05-23 10:53:37 +0800

    modify

--- 職能發展學院/lesson2_2.py
+++ 職能發展學院/lesson2_2.py
@@ -1,10 +1,14 @@
-import tkinter
+import tkinter as tk
+import tkinter.font as tkFont
 
 def main():
-    window = tkinter.Tk()
+    window = tk.Tk()
     window.title("這是我的第一個視窗")
     window.geometry("300x300")
     window.resizable(width=0, height=0)
+    font=tkFont.Font(family="Lucida Grande", size=20)
+    label = tk.Label(window,text="這是我的第一個視窗",bg="#cccccc",font=font)
+    label.pack(fill="x")
     window.mainloop()
 
 if __name__ == "__main__":
