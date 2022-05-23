73449ad1eefe9953799681d5766c2f29445dc2fc
徐國堂 <roberthsu2003@gmail.com>
2022-05-23 10:05:48 +0800

    modify

--- 職能發展學院/lesson2_2.py
+++ 職能發展學院/lesson2_2.py
@@ -2,6 +2,9 @@ import tkinter
 
 def main():
     window = tkinter.Tk()
+    window.title("這是我的第一個視窗")
+    window.geometry("300x300")
+    window.resizable(width=0, height=0)
     window.mainloop()
 
 if __name__ == "__main__":
