5ba1246839fc819bd78821fb53062dfa2ca7b35f
徐國堂 <roberthsu2003@gmail.com>
2022-05-23 11:07:35 +0800

    modify

--- 職能發展學院/lesson2_2.py
+++ 職能發展學院/lesson2_2.py
@@ -6,10 +6,19 @@ def main():
     window.title("這是我的第一個視窗")
     window.geometry("300x300")
     window.resizable(width=0, height=0)
-    font=tkFont.Font(family="Lucida Grande", size=20)
-    label = tk.Label(window,text="這是我的第一個視窗",bg="#cccccc",font=font)
-    label.pack(fill="x")
+    createTopLabel(window)
+    createBottomLabel(window)
     window.mainloop()
+
+def createTopLabel(root):
+    font = tkFont.Font(family="Lucida Grande", size=20)
+    label = tk.Label(root, text="這是我的第一個視窗", bg="#cccccc", font=font)
+    label.pack(side="top",fill="x")
+
+def createBottomLabel(root):
+    font = tkFont.Font(family="Lucida Grande", size=25)
+    label = tk.Label(root, text="這是下方的文字", bg="#cccccc", font=font)
+    label.pack(side="bottom",fill="x")
 
 if __name__ == "__main__":
     main()
