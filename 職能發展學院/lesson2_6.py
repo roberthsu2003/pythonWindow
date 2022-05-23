763a4258c185702cb96f66db491649e62f6aeff4
徐國堂 <roberthsu2003@gmail.com>
2022-05-23 16:15:50 +0800

    modify

--- 職能發展學院/lesson2_6.py
+++ 職能發展學院/lesson2_6.py
@@ -4,9 +4,18 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.option_add("*font",('verdana', 20, 'bold'))
-        tk.Button(self, text="Top",padx=20,pady=20).pack(side=tk.LEFT,padx=10,pady=10)
-        tk.Button(self, text="Center",padx=20,pady=20).pack(side=tk.LEFT,padx=10,pady=10)
-        tk.Button(self, text="Button",padx=20,pady=20).pack(side=tk.LEFT,padx=10,pady=10)
+        tk.Button(self, text="LEFT",padx=20,pady=20,command=self.left_click).pack(side=tk.LEFT,padx=10,pady=10)
+        tk.Button(self, text="CENTER",padx=20,pady=20,command=self.center_click).pack(side=tk.LEFT,padx=10,pady=10)
+        tk.Button(self, text="RIGHT",padx=20,pady=20,command=self.right_click).pack(side=tk.LEFT,padx=10,pady=10)
+
+    def left_click(self):
+        print("left_click")
+
+    def center_click(self):
+        print("center_click")
+
+    def right_click(self):
+        print("right_click")
 
 
 if __name__ == "__main__":
