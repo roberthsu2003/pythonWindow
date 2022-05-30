cc4a78628a93020e302b0de82a82f6db581e063d
徐國堂 <roberthsu2003@gmail.com>
2022-05-30 10:08:50 +0800

    modify

--- 職能發展學院/lesson3_2/main.py
+++ 職能發展學院/lesson3_2/main.py
@@ -1,2 +1,11 @@
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+
 if __name__ == "__main__":
-    print("主程式")
+    root = Window()
+    root.title("這是我的視窗")
+    root.mainloop()
