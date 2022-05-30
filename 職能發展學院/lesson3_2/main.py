d4d42b85e337741c03d322ce30ace165bfcf9ef2
徐國堂 <roberthsu2003@gmail.com>
2022-05-30 14:12:03 +0800

    modify

--- 職能發展學院/lesson3_2/main.py
+++ 職能發展學院/lesson3_2/main.py
@@ -4,13 +4,18 @@ import dataSource
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        topFrame = tk.Frame(self,bg="#cccccc",borderwidth=2,relief="groove")
+        titleFrame = tk.Frame(self, bg="#333333",borderwidth=2,relief=tk.SUNKEN,padx=50,pady=50)
+        tk.Label(titleFrame,text="台北市youbike2.0即時資訊",bg="#333333",fg="#cccccc",font=('arial',20)).pack()
+        updateButton = tk.Button(titleFrame,text="立即更新",bg="#333333",fg="#333333",font=('arial',16),command=self.updateData)
+        updateButton.pack(pady=(20,0))
+        titleFrame.pack(pady=20)
+
         col = 4
         for i in range(len(dataSource.AREA)):
             if  i % col == 0:
                 topFrame = tk.Frame(self, bg="#cccccc", borderwidth=2, relief="groove")
                 topFrame.pack(padx=20, pady=20)
-            btn1 = tk.Button(topFrame, text=dataSource.AREA[i], command=self.btn1Click, padx=20, pady=20)
+            btn1 = tk.Button(topFrame, text=dataSource.AREA[i], padx=20, pady=20)
             btn1.pack(side=tk.LEFT, padx=20, pady=20)
 
 
@@ -18,16 +23,11 @@ class Window(tk.Tk):
 
 
 
-    def btn1Click(self):
+    def updateData(self):
         youbikeList =dataSource.download()
         for site in youbikeList:
             print(site)
 
-    def btn2Click(self):
-        print("btn2 click")
-
-    def btn3Click(self):
-        print("btn3 click")
 
 
 if __name__ == "__main__":
