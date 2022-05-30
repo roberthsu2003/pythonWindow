c7c549458a9515cb96240c792274caeee164ae58
徐國堂 <roberthsu2003@gmail.com>
2022-05-30 11:42:01 +0800

    modify

--- 職能發展學院/lesson3_2/dataSource.py
+++ 職能發展學院/lesson3_2/dataSource.py
@@ -1,5 +1,6 @@
 import requests
 
+AREA = ["文山區","內湖區","南港區","萬華區","大安區","中正區","松山區","信義區","北投區","大同區","士林區","中山區"]
 def download():
     url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
     response = requests.get(url)
--- 職能發展學院/lesson3_2/main.py
+++ 職能發展學院/lesson3_2/main.py
@@ -4,15 +4,16 @@ import dataSource
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
+
+
         topFrame = tk.Frame(self,bg="#cccccc",borderwidth=2,relief="groove")
-        btn1 = tk.Button(topFrame,text="下載1",command=self.btn1Click,padx=20,pady=20)
-        btn1.pack(side=tk.LEFT,padx=20,pady=20)
+        for area in dataSource.AREA:
+            btn1=tk.Button(topFrame,text=area,command=self.btn1Click,padx=20,pady=20)
+            btn1.pack(side=tk.LEFT,padx=20,pady=20)
 
-        btn2 = tk.Button(topFrame, text="下載2", command=self.btn2Click, padx=20, pady=20)
-        btn2.pack(side=tk.LEFT,padx=20, pady=20)
 
-        btn3 = tk.Button(topFrame, text="下載3", command=self.btn3Click, padx=20, pady=20)
-        btn3.pack(side=tk.LEFT, padx=20, pady=20)
+
+
         topFrame.pack(padx=20,pady=20)
 
     def btn1Click(self):
