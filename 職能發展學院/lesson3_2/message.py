ecf7d72aecd8e66fd2c6d1be3e459f66e1c19415
徐國堂 <roberthsu2003@gmail.com>
2022-06-10 10:03:48 +0800

    modify

--- 職能發展學院/lesson3_2/message.py
+++ 職能發展學院/lesson3_2/message.py
@@ -8,15 +8,23 @@ class MapDialog(Dialog):
         super().__init__(parent,title=title)
 
 
+    def getCenter(self):
+
+        return 25.038128318756307,121.56306490172479
+
     def body(self, master):
         map_widget = tkmap.TkinterMapView(master,
                                           width=800,
                                           height=600,
                                           corner_radius=0
                                           )
-        map_widget.set_position(25.038128318756307, 121.56306490172479)  # 台北市位置
+        centerLat,centerLong = self.getCenter()
+
+        map_widget.set_position(centerLat, centerLong)  # 台北市位置
         map_widget.set_zoom(15)  # 設定顯示大小
         map_widget.pack()
+        print(self.info)
+
 
     def buttonbox(self):
         #super().buttonbox()
