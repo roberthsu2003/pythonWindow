b2f1ff0d04d2f891b75c4c0ba5ba12be72458af5
徐國堂 <roberthsu2003@gmail.com>
2022-06-10 10:57:33 +0800

    modify

--- 職能發展學院/lesson3_2/message.py
+++ 職能發展學院/lesson3_2/message.py
@@ -9,8 +9,25 @@ class MapDialog(Dialog):
 
 
     def getCenter(self):
+        lat_l,lat_s,lng_l,lng_s = -10000,100000,-100000,100000
+        for site in self.info:
+            if lat_l < site["lat"]:
+                lat_l = site["lat"]
+            if lat_s > site["lat"]:
+                lat_s = site["lat"]
+            if lng_l < site["lng"]:
+                lng_l = site["lng"]
+            if lng_s > site["lng"]:
+                lng_s = site["lng"]
 
-        return 25.038128318756307,121.56306490172479
+        lat_cen = lat_s + ((lat_l - lat_s) / 2)
+        lng_cen = lng_s + ((lng_l - lng_s) / 2)
+        print(lat_cen)
+        print(lng_cen)
+        return lat_cen, lng_cen
+
+
+
 
     def body(self, master):
         map_widget = tkmap.TkinterMapView(master,
@@ -21,9 +38,9 @@ class MapDialog(Dialog):
         centerLat,centerLong = self.getCenter()
 
         map_widget.set_position(centerLat, centerLong)  # 台北市位置
-        map_widget.set_zoom(15)  # 設定顯示大小
+        map_widget.set_zoom(13)  # 設定顯示大小
         map_widget.pack()
-        print(self.info)
+        #print(self.info)
 
 
     def buttonbox(self):
