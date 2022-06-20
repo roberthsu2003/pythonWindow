567f449b9643fbc6daacf0bf21531a5f7598b372
徐國堂 <roberthsu2003@gmail.com>
2022-06-20 10:07:19 +0800

    modify

--- 職能發展學院/lesson5_1.py
+++ 職能發展學院/lesson5_1.py
@@ -4,10 +4,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get('http://pythonscraping.com/pages/javascript/ajaxDemo.html')
-time.sleep(3)
-print(driver.page_source)
-driver.close()
+def getData(stock_number):
+    url = "https://mis.twse.com.tw/stock/fibest.jsp?stock=" + stock_number
+    driver.get(url)
+    time.sleep(3)
+    print(driver.page_source)
+    driver.close()
+
+
+if __name__ == "__main__":
+    getData("2330")
 
 
 
