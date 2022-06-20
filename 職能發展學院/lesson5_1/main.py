4f994126de38e6075e3a15c75f83d449fbd7a856
徐國堂 <roberthsu2003@gmail.com>
2022-06-20 11:30:20 +0800

    modify

--- 職能發展學院/lesson5_1/main.py
+++ 職能發展學院/lesson5_1/main.py
@@ -1,14 +1,27 @@
 from dataSource import getData
+from dataSource import driver
+import tkinter as tk
+class Window(tk.Tk):
+    pass
 
-
+def closeWindow():
+    print("close window")
+    window.destroy()
+    driver.close()
+    driver.quit()
 
 if __name__ == "__main__":
-    title,t_odd,odd,diff_odd,percent_diff=getData("2330")
+    window = Window()
+    window.resizable(width=0,height=0)
+    window.protocol("WM_DELETE_WINDOW",closeWindow)
+    title, t_odd, odd, diff_odd, percent_diff = getData("2330")
     print(title)
     print(t_odd)
     print(odd)
     print(diff_odd)
     print(percent_diff)
+    window.mainloop()
+
 
 
 
