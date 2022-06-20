80d9143377fe362529499eec6a689ba78a8ebcba
徐國堂 <roberthsu2003@gmail.com>
2022-06-20 09:50:24 +0800

    modify

--- 職能發展學院/lesson5_1.py
+++ 職能發展學院/lesson5_1.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
-driver=webdriver.Chrome('./chromedriver')
-driver.get("https://www.google.com.tw")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get('http://pythonscraping.com/pages/javascript/ajaxDemo.html')
 time.sleep(3)
+print(driver.page_source)
 driver.close()
+
 
 
