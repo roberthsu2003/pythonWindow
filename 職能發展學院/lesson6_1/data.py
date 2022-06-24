609b700b148f62ce4f973f225d89805280a3ae3f
徐國堂 <roberthsu2003@gmail.com>
2022-06-24 10:33:59 +0800

    modify

--- 職能發展學院/lesson6_1/data.py
+++ 職能發展學院/lesson6_1/data.py
@@ -5,6 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
+from datetime import datetime
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.set_window_position(x=-10000, y=-10000)
@@ -22,17 +23,18 @@ def getData(stock_number):
 
     driver.find_element(By.ID,"btnChangeToOdd").click()
     time.sleep(1)
-
+    now  = datetime.now()
+    now_str = now.strftime("%Y-%m-%d ")
     soup = BeautifulSoup(driver.page_source,'html.parser')
     title = soup.find(id=stock_number+"_n",class_="title").string
     t_odd = soup.find(id=stock_number+"_t_odd", class_="oddObj").string
     odd = soup.find(id=stock_number+"_z_odd", class_="oddObj").string
     diff_odd = soup.find(id=stock_number + "_diff_odd").string
     percent_diff = soup.find(id=stock_number + "_pre_odd").string
-    return title,t_odd,odd,diff_odd,percent_diff
+    return title,(now_str+t_odd),odd,diff_odd,percent_diff
 
 
-print(getData("2330"))
+print(getData("2809"))
 driver.close()
 driver.quit()
 
