1f563fd620e30e371454e47feb0e467549626121
徐國堂 <roberthsu2003@gmail.com>
2022-06-24 10:03:54 +0800

    modify

--- 職能發展學院/lesson6_1/data.py
+++ 職能發展學院/lesson6_1/data.py
@@ -1,5 +1,42 @@
 import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
 
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.set_window_position(x=-10000, y=-10000)
+def getData(stock_number):
+    url = "https://mis.twse.com.tw/stock/fibest.jsp?stock=" + stock_number
+    try:
+        if driver.current_url == url:
+            driver.refresh()
+        else:
+            driver.get(url)
+    except Exception as e:
+        print(f"伺服器發生錯誤:{e}")
+        raise Exception()
+
+
+    driver.find_element(By.ID,"btnChangeToOdd").click()
+    time.sleep(1)
+
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+    title = soup.find(id=stock_number+"_n",class_="title").string
+    t_odd = soup.find(id=stock_number+"_t_odd", class_="oddObj").string
+    odd = soup.find(id=stock_number+"_z_odd", class_="oddObj").string
+    diff_odd = soup.find(id=stock_number + "_diff_odd").string
+    percent_diff = soup.find(id=stock_number + "_pre_odd").string
+    return title,t_odd,odd,diff_odd,percent_diff
+
+
+print(getData("2330"))
+driver.close()
+driver.quit()
+
+'''
 if not os.path.isdir('assets'):
     os.mkdir('assets')
 
@@ -7,3 +44,4 @@ abs_file_name = os.path.abspath('./assets/oop.txt')
 file  = open(abs_file_name,'w',encoding='utf-8')
 print("Hello! Python!",file=file)
 file.close()
+'''
