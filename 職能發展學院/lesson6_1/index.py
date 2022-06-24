4b553895aa78caaf2bd99cf5a43f91796b7dbe1a
徐國堂 <roberthsu2003@gmail.com>
2022-06-24 16:01:26 +0800

    modify

--- 職能發展學院/lesson6_1/index.py
+++ 職能發展學院/lesson6_1/index.py
@@ -1,27 +1,33 @@
 from tkinter import messagebox
+import tkinter.ttk as ttk
 
 import tkinter as tk
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.currentStockID = ""
-        self.t = None
-        self.title("股票成交價及時查詢提醒系統")
+        self.title("台積電,聯電,聯發科,鴻海歷史線圖")
         mainFrame  = tk.Frame(self, relief="groove", borderwidth=2)
         titleFrame = tk.Frame(mainFrame)
-        tk.Label(titleFrame,text="股票成交價及時查詢提醒系統",font=("Arial",20,'bold'),fg="#555555").pack(padx=10)
+        tk.Label(titleFrame,text="台積電,聯電,聯發科,鴻海歷史線圖",font=("Arial",20,'bold'),fg="#555555").pack(padx=10)
         titleFrame.pack(pady=30)
         mainFrame.pack(pady=30,padx=30,ipadx=30,ipady=30)
 
         #-------------建立inputFrame-------------------
         self.inputFrame = tk.Frame(mainFrame,width=50)
-        tk.Label(self.inputFrame, text="輸入欲查詢的股票號碼:",font=('Arial',13)).grid(row=0,column=0,sticky=tk.E)
-        self.stockIDentry  = tk.Entry(self.inputFrame,text=tk.StringVar(),bd=5)
-        self.stockIDentry.grid(row=0, column=1, sticky=tk.E)
-        subminButton  = tk.Button(self.inputFrame, font=('Arial',15),text="搜尋")
-        subminButton.grid(row=0, column=2, sticky=tk.E)
-        self.inputFrame.pack()
+        tk.Label(self.inputFrame, text="請選取股票號碼:",font=('Arial',13)).grid(row=0,column=0,sticky=tk.E)
+        n = tk.StringVar()
+        stock_choose = ttk.Combobox(self.inputFrame, width=27, textvariable=n)
 
+        # Adding combobox drop down list
+        stock_choose['values'] = ('2330.TW',
+                                  '2303.TW',
+                                  '2454.TW',
+                                  '2317.TW',
+                                  )
+
+        stock_choose.grid(row=0, column=1, sticky=tk.E)
+
+        self.inputFrame.pack()
         #-----------建立顯示畫面-----------------------
 
 
