1a5840051c09cf03a9c6978b52c9e9f6156a3d07
徐國堂 <roberthsu2003@gmail.com>
2022-06-28 15:59:19 +0800

    modify

--- 職能發展學院/lesson7_1/data.py
+++ 職能發展學院/lesson7_1/data.py
@@ -2,6 +2,8 @@ from urllib.request import Request, urlopen
 import ssl
 from bs4 import BeautifulSoup
 import time
+from datetime import datetime
+import csv
 ssl._create_default_https_context = ssl._create_unverified_context
 heads = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
 
@@ -14,11 +16,25 @@ while True:
     all_groups=soup.find_all('td',attrs={"class":"views-field views-field-field-crypto-price-1 views-align-right"})
     bit_object = all_groups[0]
     ethe_object = all_groups[1]
-    print(bit_object.string)
-    print(ethe_object.string)
+    bit=bit_object.string
+    bit = bit.strip()
+    ethe=ethe_object.string
+    ethe = ethe.strip()
     all_changes = soup.find_all('td',attrs={"class":"views-field views-field-field-crypto-price-change-pc-24h views-align-right"})
     bit_change_object = all_changes[0]
     ethe_change_object = all_changes[1]
-    print(bit_change_object.string)
-    print(ethe_change_object.string)
+    bit_change=bit_change_object.string
+    bit_change = bit_change.strip()
+    ethe_change=ethe_change_object.string
+    ethe_change=ethe_change.strip()
+    #存檔
+    now = datetime.now()
+    file_name = now.strftime("%Y-%m-%d_crypto.csv")
+    now_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    with open(file_name,'a',encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["bitcoin",now_string,bit,bit_change])
+
+        writer.writerow(["Ethereum",now_string,ethe,ethe_change])
+
     time.sleep(60)
