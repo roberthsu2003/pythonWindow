7845acfe102b934eb003d5ae2ff1e0cf96f68224
徐國堂 <roberthsu2003@gmail.com>
2022-10-18 11:51:05 +0800

    as tk

--- _11109Python/專案1/main.py
+++ _11109Python/專案1/main.py
@@ -1,9 +1,9 @@
-import tkinter
+import tkinter as tk
 
 def createWindow():
-    window = tkinter.Tk()
+    window = tk.Tk()
     window.title("這是我的第一個視窗")
-    btn = tkinter.Button(window,text="請按我",padx=20,pady=20,font=('arial',16))
+    btn = tk.Button(window,text="請按我",padx=20,pady=20,font=('arial',16))
     btn.pack(padx=50,pady=30)
     window.mainloop()
 
