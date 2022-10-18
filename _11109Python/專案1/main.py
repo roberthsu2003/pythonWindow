2113ae0f55e3763432712ebb19faa12328473829
徐國堂 <roberthsu2003@gmail.com>
2022-10-18 11:47:38 +0800

    建立btn,設定btn屬性

--- _11109Python/專案1/main.py
+++ _11109Python/專案1/main.py
@@ -3,6 +3,8 @@ import tkinter
 def createWindow():
     window = tkinter.Tk()
     window.title("這是我的第一個視窗")
+    btn = tkinter.Button(window,text="請按我",padx=20,pady=20,font=('arial',16))
+    btn.pack(padx=50,pady=30)
     window.mainloop()
 
 def main():
