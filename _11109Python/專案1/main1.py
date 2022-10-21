a06a8f3ba4a4449bca31a2824bf9c66e957929e7
徐國堂 <roberthsu2003@gmail.com>
2022-10-21 14:56:37 +0800

    modify

---
+++ _11109Python/專案1/main1.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        mainFrame = tk.Frame(self,bg='#345678',width=800,height=500)
+        mainFrame.pack()
+
+
+def main():
+    window = Window()
+    window.title("Frame框架")
+    window.geometry("800x500")
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
