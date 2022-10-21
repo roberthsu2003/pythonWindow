92f59c1e1529775edbb76bc95ff25fc849a4d155
徐國堂 <roberthsu2003@gmail.com>
2022-10-21 14:17:09 +0800

    modify

--- _11109Python/專案1/main.py
+++ _11109Python/專案1/main.py
@@ -1,14 +1,17 @@
 import tkinter as tk
 
-def createWindow():
-    window = tk.Tk()
-    window.title("這是我的第一個視窗")
-    btn = tk.Button(window,text="請按我",padx=20,pady=20,font=('arial',16))
-    btn.pack(padx=50,pady=30)
-    window.mainloop()
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("這是我的第一個觀窗")
+        btn = tk.Button(self,text="請按我",padx=20,pady=20,font=('arial',16))
+        btn.pack(padx=50,pady=30)
+
 
 def main():
-    createWindow()
+    window = Window()
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
