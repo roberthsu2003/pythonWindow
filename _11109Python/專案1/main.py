6c720d36ae8998bfb0e91b1154bff5273e484d45
徐國堂 <roberthsu2003@gmail.com>
2022-10-21 14:31:34 +0800

    modify

--- _11109Python/專案1/main.py
+++ _11109Python/專案1/main.py
@@ -5,8 +5,11 @@ class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("這是我的第一個觀窗")
-        btn = tk.Button(self,text="請按我",padx=20,pady=20,font=('arial',16))
-        btn.pack(padx=50,pady=30)
+        tk.Button(self,text="請按我",padx=20,pady=20,font=('arial',16),command=self.userClick).pack(padx=50,pady=30)
+
+    def userClick(self):
+        print("userClick")
+        
 
 
 def main():
