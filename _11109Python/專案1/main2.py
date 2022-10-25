0b45f8fa2f240d44cece971cd4a6ffc777dd811d
徐國堂 <roberthsu2003@gmail.com>
2022-10-25 13:41:48 +0800

    window大小設定

--- _11109Python/專案1/main2.py
+++ _11109Python/專案1/main2.py
@@ -15,7 +15,8 @@ class Window(tk.Tk):
 def main():
     window = Window()
     window.title("Frame框架")
-    window.geometry("640x427")
+    window.resizable(0,0) #可以使用滑鼠改變視窗的大小
+    window.geometry("640x427+300+200")#設定window的大小和位置
     window.mainloop()
 
 if __name__ == "__main__":
