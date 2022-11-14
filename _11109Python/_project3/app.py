779a9f894a94d48a7df6ada2c3e1b388cbcbdd4c
徐國堂 <roberthsu2003@gmail.com>
2022-11-14 15:52:38 +0800

    modify

--- _11109Python/_project3/app.py
+++ _11109Python/_project3/app.py
@@ -1,2 +1,17 @@
 import private
-print(private.secret.open_weather_key)
+import requests
+
+def main():
+    api_key = private.secret.open_weather_key
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q=Hualien,tw&APPID={api_key}&lang=zh_tw&units=metric"
+    response = requests.get(url)
+    if response.ok :
+        print("下載成功")
+        allData = response.json()
+        city = allData['city']
+        print(city)
+
+
+
+if __name__ == "__main__":
+    main()
