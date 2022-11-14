176d876c7402631c87dac4a06b2ea50871144dca
徐國堂 <roberthsu2003@gmail.com>
2022-11-14 16:36:02 +0800

    modify

--- _11109Python/_project3/app.py
+++ _11109Python/_project3/app.py
@@ -5,7 +5,12 @@ import datasource
 
 def main():
     api_key = private.secret.open_weather_key
-    city = datasource.get_forcase_data("Taipei",api_key)
+    try:
+        city = datasource.get_forcase_data("Taipei",api_key)
+    except Exception as e:
+        print(e)
+        return
+
     print(city)
     
 
--- _11109Python/_project3/datasource.py
+++ _11109Python/_project3/datasource.py
@@ -2,10 +2,11 @@
 import requests
 
 def get_forcase_data(cityName,api_key):
-    url = f"https://api.openweathermap.org/data/2.5/forecast?q={cityName},tw&APPID={api_key}&lang=zh_tw&units=metric"
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={cityName},tw&APP={api_key}&lang=zh_tw&units=metric"
     response = requests.get(url)
     if response.ok :
         print("下載成功")
         allData = response.json()
         city = allData['city']
         return city
+    raise Exception("下載失敗")
