de36ca47918b1ca7cea4a1335eb993dde5818642
徐國堂 <roberthsu2003@gmail.com>
2022-11-14 16:19:13 +0800

    modify

--- _11109Python/_project3/app.py
+++ _11109Python/_project3/app.py
@@ -1,18 +1,11 @@
 import private
-import requests
+import datasource
 
-def get_forcase_data(cityName,api_key):
-    url = f"https://api.openweathermap.org/data/2.5/forecast?q={cityName},tw&APPID={api_key}&lang=zh_tw&units=metric"
-    response = requests.get(url)
-    if response.ok :
-        print("下載成功")
-        allData = response.json()
-        city = allData['city']
-        return city
+
 
 def main():
     api_key = private.secret.open_weather_key
-    city = get_forcase_data("Taipei",api_key)
+    city = datasource.get_forcase_data("Taipei",api_key)
     print(city)
     
 
