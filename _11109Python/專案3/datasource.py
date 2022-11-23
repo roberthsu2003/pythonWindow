fd7e891b272fe4a21c2632ada38ed1ddec9515be
徐國堂 <roberthsu2003@gmail.com>
2022-11-23 11:16:33 +0800

    modify

--- _11109Python/專案3/datasource.py
+++ _11109Python/專案3/datasource.py
@@ -1,1 +1,14 @@
+import requests
+
 api_key = "29c4f184354b9889e87f7b494ac86aed"
+cityName = "Taipei"
+
+def get_forcast_data():
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={cityName},tw&APPID={api_key}&lang=zh_tw&units=metric"
+
+    response = requests.get(url=url)
+    if response.ok:
+        print("下載成功")
+        print(response.text)
+    else:
+        print("下載失敗")
--- _11109Python/專案3/main.py
+++ _11109Python/專案3/main.py
@@ -2,7 +2,9 @@ import datasource
 
 def main():
     print("這裏是main function")
+    datasource.get_forcast_data()
 
 if __name__ == "__main__":
     print("這裏是程式的執行點")
     main()
+    
