2a892b09e1d804b3acf38eba74005ff1a1a96a8d
徐國堂 <roberthsu2003@gmail.com>
2022-11-23 16:18:27 +0800

    modify

--- _11109Python/專案3/main.py
+++ _11109Python/專案3/main.py
@@ -3,20 +3,26 @@ from secrets import api_key
 import tkinter as tk
 
 class Window(tk.Tk):
-    def __init__(self):
+    def __init__(self,cities_dict):
         super().__init__()
         tk.Label(self,text="各縣市4天天氣預測",font=('Arial', 20)).pack(padx=30,pady=30)
 
         #建立存放按鈕的容器
         buttons_frame = tk.Frame(self,background="#333333",width=200,height=300)
         buttons_frame.pack()
+
+        for key in cities_dict:
+            tk.Button(buttons_frame,text=key).pack(side=tk.LEFT)
+            
+
+
         
 
         
 
 
 def main():
-    window = Window()
+    window = Window(ds.tw_county_names)
     window.title("各縣市4天天氣預測")
     window.mainloop()
 
