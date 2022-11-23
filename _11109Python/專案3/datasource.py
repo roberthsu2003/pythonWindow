54495a0ea0ddc5a809c84215fc8588b503be7e20
徐國堂 <roberthsu2003@gmail.com>
2022-11-23 13:49:53 +0800

    modify

--- _11109Python/專案3/datasource.py
+++ _11109Python/專案3/datasource.py
@@ -1,9 +1,8 @@
 import requests
 
 api_key = "29c4f184354b9889e87f7b494ac86aed"
-cityName = "Taipei"
 
-def get_forcast_data():
+def get_forcast_data(cityName):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={cityName},tw&APPID={api_key}&lang=zh_tw&units=metric"
 
     response = requests.get(url=url)
