6483fba95a5ab33c2441b86195ec7e1e4d4e5e43
徐國堂 <roberthsu2003@gmail.com>
2022-11-23 14:16:03 +0800

    modify

--- _11109Python/專案3/datasource.py
+++ _11109Python/專案3/datasource.py
@@ -21,7 +21,7 @@ tw_county_names = {"台北":"Taipei",
                    "連江":"Lienchiang"
                    }
 
-def get_forcast_data(cityName):
+def get_forcast_data(cityName,api_key):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={cityName},tw&APPID={api_key}&lang=zh_tw&units=metric"
 
     response = requests.get(url=url)
--- _11109Python/專案3/main.py
+++ _11109Python/專案3/main.py
@@ -1,9 +1,10 @@
 import datasource as ds
+from secrets import api_key
 
 
 def main():
     print("這裏是main function")
-    list_data = ds.get_forcast_data(ds.tw_county_names["基隆"])
+    list_data = ds.get_forcast_data(ds.tw_county_names["基隆"],api_key)
     for item in list_data:
         print(item['dt_txt'])
 
