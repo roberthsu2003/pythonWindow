f6574bd77d960ecaffe9f74754f1707ef7fe231d
徐國堂 <roberthsu2003@gmail.com>
2023-03-20 15:35:57 +0800

    modify

--- 上課內容/學院_11202python應用實戰班/window1/index.py
+++ 上課內容/學院_11202python應用實戰班/window1/index.py
@@ -4,7 +4,7 @@ from tkinter import Button,Frame
 class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-        Button(self,text="按鈕1",font=('Helvetica', '24'),pady=10).pack(fill=tk.X)
+        Button(self,text="按鈕1",font=('Helvetica', '24'),pady=10,command=self.btn1_click).pack(fill=tk.X)
         Button(self,text="按鈕2",font=('Helvetica', '24'),pady=10).pack(fill=tk.X)
 
         bottom_frame  = Frame(self,bg="#ffffff")
@@ -12,6 +12,9 @@ class Window(tk.Tk):
         Button(bottom_frame,text="按鈕4",font=('Helvetica', '24')).pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
         Button(bottom_frame,text="按鈕5",font=('Helvetica', '24')).pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
         bottom_frame.pack(expand=True,fill=tk.BOTH)
+
+    def btn1_click(self):
+        print("按鈕1按下")
         
 
 
