985aaf1c253054c1fdd97fd5c780987edbc6f8ab
徐國堂 <roberthsu2003@gmail.com>
2023-03-20 11:49:03 +0800

    modify

--- 上課內容/學院_11202python應用實戰班/window1/index.py
+++ 上課內容/學院_11202python應用實戰班/window1/index.py
@@ -1,1 +1,15 @@
-print("Hello! Python!")
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+
+
+def main():
+    window = Window()
+    window.title("這是第一個視窗")
+    window.geometry("400x300")
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
