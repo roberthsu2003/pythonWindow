8b1752f50f036f5fd47571bec7db3332ae45e543
徐國堂 <roberthsu2003@gmail.com>
2023-03-20 13:44:36 +0800

    modify

--- 上課內容/學院_11202python應用實戰班/window1/index.py
+++ 上課內容/學院_11202python應用實戰班/window1/index.py
@@ -1,8 +1,14 @@
 import tkinter as tk
+from tkinter import Button
 
 class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
+        btn1 = Button(self,text="按鈕1")
+        btn1.pack()
+
+        btn2 = Button(self,text="按鈕2")
+        btn2.pack()
 
 
 def main():
