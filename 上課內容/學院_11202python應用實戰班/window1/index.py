ea825883b01452a89fd5eee963d70553507731d6
徐國堂 <roberthsu2003@gmail.com>
2023-03-20 14:43:34 +0800

    modify

--- 上課內容/學院_11202python應用實戰班/window1/index.py
+++ 上課內容/學院_11202python應用實戰班/window1/index.py
@@ -1,11 +1,17 @@
 import tkinter as tk
-from tkinter import Button
+from tkinter import Button,Frame
 
 class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         Button(self,text="按鈕1",font=('Helvetica', '24'),pady=10).pack(fill=tk.X)
         Button(self,text="按鈕2",font=('Helvetica', '24'),pady=10).pack(fill=tk.X)
+
+        bottom_frame  = Frame(self,bg="#ffffff")
+        Button(bottom_frame,text="按鈕3",font=('Helvetica', '24'),pady=10).pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
+        Button(bottom_frame,text="按鈕4",font=('Helvetica', '24'),pady=10).pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
+        Button(bottom_frame,text="按鈕5",font=('Helvetica', '24'),pady=10).pack(side=tk.LEFT,fill=tk.BOTH,expand=True)
+        bottom_frame.pack(expand=True,fill=tk.BOTH)
         
 
 
