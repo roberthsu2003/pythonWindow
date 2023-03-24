85c9f28f382046f81238605d4fb92bf3e0e3441c
徐國堂 <roberthsu2003@gmail.com>
2023-03-24 11:18:49 +0800

    modify

--- 上課內容/學院_11202python應用實戰班/grid/index.py
+++ 上課內容/學院_11202python應用實戰班/grid/index.py
@@ -3,10 +3,15 @@
 '''
 
 import tkinter as tk
+from tkinter import ttk
 
 class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
+        ttkStyle = ttk.Style()
+        ttkStyle.configure('1.main_bg_Color',background='#ffffff')
+        mainFrame  =  ttk.Frame(self,style='1.main_bg_Color')
+        mainFrame.pack(expand=True,fill=tk.BOTH)
 
 
 def main():
