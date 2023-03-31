06d8c3a439530bfc7f1c8a4d1490ef7612dbb0c1
徐國堂 <roberthsu2003@gmail.com>
2023-03-31 11:12:15 +0800

    modify

--- 上課內容/學院_11202python應用實戰班/drawing/index.py
+++ 上課內容/學院_11202python應用實戰班/drawing/index.py
@@ -4,9 +4,20 @@ from tkinter import ttk
 class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-        drawingFrame = ttk.Labelframe(self,text="這裏是畫圖區")
+        ttkStyle = ttk.Style()
+        #print(ttkStyle.theme_names())
+        ttkStyle.theme_use('classic')
+        ttkStyle.configure('white.TLabelFrame',background='white')
+
+        drawingFrame = ttk.LabelFrame(self,text="這裏是畫圖區",style='white.TLabelFrame')
         drawingFrame.pack(padx=50,pady=50)
-        tk.Button(drawingFrame,text="Press Me",padx=10,pady=10).pack(padx=30,pady=20)
+        lineCanvas = tk.Canvas(drawingFrame,width=100,height=30)
+        lineCanvas.create_line((0,0),(100,0),width=30,fill='red')
+        lineCanvas.pack()
+        ovalCanvas = tk.Canvas(drawingFrame,width=110,height=110)
+        ovalCanvas.create_oval((10,10),(100,100),width=10,outline='red',fill='purple')
+        ovalCanvas.pack()
+
 
 
 
