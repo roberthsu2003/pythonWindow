282eb4f6da85f06de2c4d0aec2172e4a07dc9bbc
徐國堂 <roberthsu2003@gmail.com>
2023-04-06 09:49:22 +0800

    modify

--- 上課內容/學院_11202python應用實戰班/widgets/main.py
+++ 上課內容/學院_11202python應用實戰班/widgets/main.py
@@ -1,9 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
+#create canvas with tkinter
+#How to draw images in tkinter window
+
+def DrawImageWithTkinter():
+    root = tk.Tk()
+    root.title("Drawing an Image")
+    root.geometry("400x400")
+    canvas = tk.Canvas(root, width=400, height=400)
+    canvas.pack()
+    img = tk.PhotoImage(file="image.png")
+    canvas.create_image(20, 20, anchor=tk.NW, image=img)
+    root.mainloop()
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
+        topFrame = ttk.LabelFrame(self)
+        canvas = tk.Canvas(topFrame, width=400, height=400)
+        canvas.pack()
+        topFrame.pack()
         
 
 def main():
