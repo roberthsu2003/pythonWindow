b3367efa4c02026f3474864fe994760f184f161a
徐國堂 <roberthsu2003@gmail.com>
2023-04-06 13:45:33 +0800

    modify

--- 上課內容/學院_11202python應用實戰班/widgets/parts.py
+++ 上課內容/學院_11202python應用實戰班/widgets/parts.py
@@ -7,7 +7,7 @@ class TopFrame(ttk.LabelFrame):
         super().__init__(master,**kwargs)
         flowerImage1 = Image.open("./images/flower1.png")
         self.flowerPhoto1 = ImageTk.PhotoImage(flowerImage1)
-        self.canvas = tk.Canvas(self, width=500, height=200)
+        self.canvas = tk.Canvas(self, width=173, height=200)
         self.canvas.pack()
         self.canvas.create_image(0,5,image=self.flowerPhoto1,anchor='nw')
         self.canvas.create_text(0,200,text='Flower', fill='yellow', font=('verdana', 36),
@@ -20,5 +20,13 @@ class TopFrame(ttk.LabelFrame):
         atomImage1 = Image.open("./images/atom.png")
         self.atomPhoto1 = ImageTk.PhotoImage(atomImage1)
         self.canvas.create_image(280,5,image=self.atomPhoto1,anchor='nw')
+
+        #created ttk.scrollbar of tkinter in canvas
+        self.scrollbar = ttk.Scrollbar(self, orient='horizontal', command=self.canvas.xview)
+        self.scrollbar.pack(side='bottom', fill='x')
+        self.canvas.configure(xscrollcommand=self.scrollbar.set)
+        self.canvas.configure(scrollregion=(0,0,500,200))
+
+
 
 
