497a704bc2b8ced4b5407236dcfda31432f9edbc
徐國堂 <roberthsu2003@gmail.com>
2023-04-07 13:55:40 +0800

    modify

---
+++ 上課內容/學院_11202python應用實戰班/youbike/datasource.py
@@ -0,0 +1,6 @@
+import requests
+
+def getInfo():
+    url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+    response = requests.get(url)
+    print(response.text)
---
+++ 上課內容/學院_11202python應用實戰班/youbike/main.py
@@ -0,0 +1,7 @@
+import datasource
+
+def main():
+    datasource.getInfo()
+
+if __name__ == "__main__":
+    main()
