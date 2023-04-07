585f43b21b1ab08ef76c4d83d1eacefecbb6ace3
徐國堂 <roberthsu2003@gmail.com>
2023-04-07 15:04:59 +0800

    modify

--- 上課內容/學院_11202python應用實戰班/youbike/main.py
+++ 上課內容/學院_11202python應用實戰班/youbike/main.py
@@ -6,6 +6,12 @@ class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         topFrame = ttk.LabelFrame(self,text="台北市行政區")
+        length = len(datasource.sarea_list)
+        for i in range(length):
+            cols = i % 3
+            rows = i // 3
+            ttk.Radiobutton(topFrame,text=datasource.sarea_list[i]).grid(column=cols,row=rows)
+
         topFrame.pack()
 
 def main():
