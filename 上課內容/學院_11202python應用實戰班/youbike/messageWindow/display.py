b19d330b4ccc30a7f2a5fe13a646dd3190737e86
徐國堂 <roberthsu2003@gmail.com>
2023-04-13 11:43:17 +0800

    modify

--- 上課內容/學院_11202python應用實戰班/youbike/messageWindow/display.py
+++ 上課內容/學院_11202python應用實戰班/youbike/messageWindow/display.py
@@ -1,8 +1,17 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.simpledialog import Dialog
 
-class MapDisplay(tk.Toplevel):
+class MapDisplay(Dialog):
     def __init__(self,master,data_dict,**kwargs):
         super().__init__(master,**kwargs)
+        
+    
+    #override
+    def body(self,master):
         closeButton = ttk.Button(self,text="close")
         closeButton.pack(padx=30,pady=30)
+    #override
+    
+    def buttonbox(self):
+        pass
